af2d76e7e1507cec239fb6d798f156bf97efe3c9
lyd <2428170173@qq.com>
2024-07-28 17:09:28 +0800

    dev 开发用户前台页面 lyd

--- src/views/Front.vue
+++ src/views/Front.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
-    <div class="front-notice"><i class="el-icon-bell" style="margin-right: 2px"></i>公告：{{ top }}</div>
     <!--头部-->
     <div class="front-header">
       <div class="front-header-left">
-        <img src="@/assets/imgs/logo.png" alt="">
-        <div class="title">项目前台</div>
+        <img src="@/assets/imgs/logo1.png" alt="">
+        <div class="title">二手交易平台</div>
       </div>
       <div class="front-header-center">
-        <div class="front-header-nav">
-          <el-menu :default-active="$route.path" mode="horizontal" router>
-						<el-menu-item index="/front/home">首页</el-menu-item>
-						<el-menu-item index="/front/person">个人中心</el-menu-item>
-          </el-menu>
-        </div>
+        <div @click="$router.push(item.path)" class="menu" v-for="item in menus" :key="item.path"
+             :class="{'menu-active' : item.path === $route.path }">{{ item.text }}</div>
       </div>
       <div class="front-header-right">
         <div v-if="!user.username">
@@ -23,8 +18,8 @@
         <div v-else>
           <el-dropdown>
             <div class="front-header-dropdown">
-              <img :src="user.avatar" alt="">
-              <div style="margin-left: 10px">
+              <img :src="user.avatar" alt="" style="border-radius: 50%">
+              <div style="margin-left: 10px; color: #eee; cursor: pointer">
                 <span>{{ user.name }}</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
               </div>
             </div>
@@ -52,9 +47,15 @@ export default {
 
   data () {
     return {
-      top: '',
       notice: [],
       user: JSON.parse(localStorage.getItem("xm-user") || '{}'),
+      menus: [
+        { text: '热卖专区', path: '/front/home' },
+        { text: '社区广场', path: '/front/posts' },
+        { text: '求购专区', path: '/front/helpView' },
+        { text: '系统公告', path: '/front/notice' },
+        { text: '留言反馈', path: '/front/feedback' },
+      ],
     }
   },
 
@@ -93,4 +94,17 @@ export default {
 
 <style scoped>
   @import "@/assets/css/front.css";
+
+  .menu {
+    color: #eeeeee;
+    font-size: 16px;
+    padding: 0 20px;
+    cursor: pointer;
+  }
+  .menu:hover {
+    color: orange;
+  }
+  .menu-active {
+    color: orange;
+  }
 </style>
--- src/views/front/Home.vue
+++ src/views/front/Home.vue
@@ -1,5 +1,61 @@
 <template>
-  <div class="main-content">
+  <div style="min-height: 100vh">
+    <div class="bg">
+      <div style="width: 600px">
+        <div style="text-align: center; color: #eee; font-size: 30px; margin-bottom: 20px">好看、好用、好玩，都在这里</div>
+        <div style="display: flex">
+          <el-input v-model="name" size="medium" prefix-icon="el-icon-search" placeholder="请输入商品名称关键字搜索"></el-input>
+          <el-button @click="$router.push({ path: '/front/search', query: { name: name } })" size="medium" type="primary" style="margin-left: 5px; background-color: #409EFF"><i class="el-icon-search"></i></el-button>
+        </div>
+      </div>
+    </div>
+
+    <div style="width: 70%; background-color: #fff; margin: 10px auto; padding: 20px; border-radius: 5px; position: relative">
+
+      <el-button @click="$router.push('/front/addGoods')" style="position: absolute; top: 20px; right: -150px" size="medium" type="primary" plain>发布商品</el-button>
+      <el-button  @click="$router.push('/front/userHelp')" style="position: absolute; top: 70px; right: -150px" size="medium" type="primary" plain>发布求购</el-button>
+
+      <div style="margin-bottom: 20px">
+        <el-select v-model="category" size="medium" style="width: 200px" @change="loadGoods(1)">
+          <el-option value="全部"></el-option>
+          <el-option v-for="item in categoryList" :key="item.id" :value="item.name"></el-option>
+        </el-select>
+
+        <el-select v-model="sort" size="medium" style="width: 200px; margin-left: 10px" @change="loadGoods(1)">
+          <el-option value="最新"></el-option>
+          <el-option value="最热"></el-option>
+        </el-select>
+      </div>
+
+      <div>
+        <el-row :gutter="15">
+          <el-col :span="6" v-for="item in goodsList" :key="item.id">
+            <div style="margin-bottom: 20px" class="goods-item" @click="$router.push('/front/goodsDetail?id=' + item.id)">
+              <img :src="item.img" alt="" style="width: 100%; display: block; height: 260px; margin-bottom: 10px; border-radius: 5px">
+              <div style="font-size: 16px; height: 40px; color: #555; margin-bottom: 10px" class="line2 goods-name">{{ item.name }}</div>
+              <div style="display: flex; align-items: baseline">
+                <strong style="color: red; font-size: 24px">￥{{ item.price }}</strong>
+                <span style="margin-left: 20px; color: #666">{{ item.readCount }}人浏览</span>
+                <span style="margin-left: 20px; color: #666">{{ item.likesCount }}人点赞</span>
+              </div>
+            </div>
+          </el-col>
+        </el-row>
+
+        <div style="margin: 15px auto">
+          <el-pagination
+              background
+              @current-change="handleCurrentChange"
+              :current-page="pageNum"
+              :page-sizes="[5, 10, 20]"
+              :page-size="pageSize"
+              layout="total, prev, pager, next"
+              :total="total">
+          </el-pagination>
+        </div>
+      </div>
+
+    </div>
 
   </div>
 </template>
@@ -10,15 +66,76 @@ export default {
 
   data() {
     return {
-
+      categoryList: [],
+      goodsList: [],  // 所有的数据
+      pageNum: 1,   // 当前的页码
+      pageSize: 12,  // 每页显示的个数
+      total: 0,
+      category: '全部',
+      sort: '最新',
+      name: ''
     }
   },
   mounted() {
-
+    this.loadCategory()
+    this.loadGoods(1)
   },
   // methods：本页面所有的点击事件或者其他函数定义区
   methods: {
-
+    loadCategory() {
+      this.$request.get('/category/selectAll').then(res => {
+        this.categoryList = res.data || []
+      })
+    },
+    loadGoods(pageNum) {  // 分页查询
+      if (pageNum) this.pageNum = pageNum
+      this.$request.get('/goods/selectFrontPage', {
+        params: {
+          pageNum: this.pageNum,
+          pageSize: this.pageSize,
+          category: this.category === '全部' ? null : this.category,
+          sort: this.sort
+        }
+      }).then(res => {
+        if (res.code === '200') {
+          this.goodsList = res.data?.list
+          this.total = res.data?.total
+        } else {
+          this.$message.error(res.msg)
+        }
+      })
+    },
+    handleCurrentChange(pageNum) {
+      this.loadGoods(pageNum)
+    },
   }
 }
 </script>
+
+<style scoped>
+.bg {
+  height: 400px;
+  background-image: url("@/assets/imgs/home.jpg");
+  background-size: 100%;
+  background-position-y: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.goods-item {
+  cursor: pointer;
+}
+
+.goods-item img, .goods-name {
+  transition: all .2s;
+}
+
+.goods-item:hover img {
+  scale: 1.03;
+}
+
+.goods-item:hover .goods-name {
+  color: #ec3d3d !important;
+}
+</style>
